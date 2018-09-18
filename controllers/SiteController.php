98ee377fe587966e677ea2b89c76c296f5abc406
ratkin <ratkin@prodex.net.ua>
2018-09-18 16:46:13 +0300

    shop#1

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -1,9 +1,18 @@
 <?php 
 
+include_once ROOT. '/models/Category.php';
+include_once ROOT. '/models/Product.php';
+
 class SiteController 
 {
     public function actionIndex()
     {
+        $categories = array();
+        $categories = Category::getCategoriesList();
+
+        $latestProducts = array();
+        $latestProducts = Product::getLatestProducts(8);
+        
         require_once(ROOT . '/views/site/index.php');
 
         return true;
